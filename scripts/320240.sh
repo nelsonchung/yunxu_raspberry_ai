8de0daec9cbf0afb1bf3ab84a6a1f00ab61947e8
Nelson Chung <chihchun.chung@gmail.com>
2026-04-01 23:04:18 +0800

    add 320240.sh to test the performance

---
+++ scripts/320240.sh
@@ -0,0 +1,11 @@
+./scripts/start_rpi_client.sh \
+  --camera-mode pi \
+  --camera-width 640 \
+  --camera-height 480 \
+  --model-mode ollama \
+  --ollama-base-url http://192.168.8.166:11434 \
+  --ollama-model qwen3.5:2b \
+  --ollama-timeout 300 \
+  --debug-save-frame-path /tmp/latest-frame.jpg \
+  --max-iterations 1
+
